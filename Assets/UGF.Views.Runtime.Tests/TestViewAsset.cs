9a38631b368a5b3aec48baf6f5bec992bcc87af2
Vorobey <vorobyovvorobyov@gmail.com>
2022-12-21 19:44:23 +0100

    Add tests

---
+++ Assets/UGF.Views.Runtime.Tests/TestViewAsset.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace UGF.Views.Runtime.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestViewAsset")]
+    public class TestViewAsset : ViewDescribedAsset<TestView, ViewDescription>
+    {
+        protected override ViewDescription OnBuildDescription()
+        {
+            return new ViewDescription();
+        }
+
+        protected override TestView OnBuild(ViewDescription description)
+        {
+            return new TestView(description);
+        }
+    }
+
+    public class TestView : ViewDescribed<ViewDescription>
+    {
+        public TestView(ViewDescription description) : base(description)
+        {
+        }
+    }
+}

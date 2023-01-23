57bda42aae0d661c02b4a217d692f7a74de2e373
Vorobey <vorobyovvorobyov@gmail.com>
2023-01-23 20:25:40 +0100

    Add IViewComponent

---
+++ Packages/UGF.Views/Runtime/IViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UGF.Views.Runtime
+{
+    public interface IViewComponent : IView
+    {
+        Component Component { get; }
+        bool HasComponent { get; }
+
+        void SetView(Component component);
+        void ClearView();
+    }
+}

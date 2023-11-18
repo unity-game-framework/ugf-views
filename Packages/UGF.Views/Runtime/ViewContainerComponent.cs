2c90d2e998848d64d8ca36425dce6e68d27b0db5
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-18 15:49:33 +0300

    Add ViewContainerComponent

---
+++ Packages/UGF.Views/Runtime/ViewContainerComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UGF.RuntimeTools.Runtime.Containers;
+using UnityEngine;
+
+namespace UGF.Views.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Views/View Container", 2000)]
+    public class ViewContainerComponent : MonoBehaviour
+    {
+        public ContainerComponent Instance { get { return m_container ? m_container : throw new ArgumentException("Value not specified."); } }
+        public bool HasInstance { get { return m_container != null; } }
+
+        private ContainerComponent m_container;
+
+        public void Set(ContainerComponent container)
+        {
+            m_container = container ? container : throw new ArgumentNullException(nameof(container));
+        }
+
+        public void Clear()
+        {
+            m_container = default;
+        }
+    }
+}

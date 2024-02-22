d00297fb0ff3b97dd19d0917ea77c701f4c34f85
Vorobey <vorobyovvorobyov@gmail.com>
2024-02-22 15:41:48 +0300

    Add ViewValueComponent

---
+++ Packages/UGF.Views/Runtime/ViewValueComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace UGF.Views.Runtime
+{
+    public abstract class ViewValueComponent<TValue> : MonoBehaviour
+    {
+        public TValue Value { get { return HasValue ? m_value : throw new ArgumentException("Value not specified."); } }
+        public bool HasValue { get; private set; }
+
+        private TValue m_value;
+
+        public void Set(TValue value)
+        {
+            if (!OnValueValidate(value)) throw new ArgumentException("Value should be valid.", nameof(value));
+
+            m_value = value;
+
+            HasValue = true;
+        }
+
+        public void Clear()
+        {
+            m_value = default;
+
+            HasValue = false;
+        }
+
+        protected virtual bool OnValueValidate(TValue value)
+        {
+            return true;
+        }
+    }
+}

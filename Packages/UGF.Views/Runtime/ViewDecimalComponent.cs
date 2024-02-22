c5142ed1e1aab2d5714c6f63a13de0409c94c56a
Vorobey <vorobyovvorobyov@gmail.com>
2024-02-22 15:41:59 +0300

    Add view components

---
+++ Packages/UGF.Views/Runtime/ViewDecimalComponent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UGF.Views.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Views/View Decimal", 2000)]
+    public class ViewDecimalComponent : ViewValueComponent<double>
+    {
+    }
+}
---
+++ Packages/UGF.Views/Runtime/ViewObjectComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UGF.Views.Runtime
+{
+    [AddComponentMenu("Unity Game Framework/Views/View Object", 2000)]
+    public class ViewObjectComponent : ViewValueComponent<Object>
+    {
+        protected override bool OnValueValidate(Object value)
+        {
+            return value != null;
+        }
+    }
+}

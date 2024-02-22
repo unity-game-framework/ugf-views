b8423d0de5924ed3618ed2dedbeab9abbb558afe
Vorobey <vorobyovvorobyov@gmail.com>
2024-02-22 15:41:52 +0300

    Update ViewIdComponent.cs

--- Packages/UGF.Views/Runtime/ViewIdComponent.cs
+++ Packages/UGF.Views/Runtime/ViewIdComponent.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using UGF.EditorTools.Runtime.Ids;
+﻿using UGF.EditorTools.Runtime.Ids;
 using UnityEngine;
 
 namespace UGF.Views.Runtime
 {
     [AddComponentMenu("Unity Game Framework/Views/View Id", 2000)]
-    public class ViewIdComponent : MonoBehaviour
+    public class ViewIdComponent : ViewValueComponent<GlobalId>
     {
-        public GlobalId Id { get { return m_id ?? throw new ArgumentException("Value not specified."); } }
-        public bool HasId { get { return m_id.HasValue; } }
-
-        private GlobalId? m_id;
-
-        public void Set(GlobalId id)
+        protected override bool OnValueValidate(GlobalId value)
         {
-            if (!id.IsValid()) throw new ArgumentException("Value should be valid.", nameof(id));
-
-            m_id = id;
-        }
-
-        public void Clear()
-        {
-            m_id = null;
+            return value.IsValid();
         }
     }
 }

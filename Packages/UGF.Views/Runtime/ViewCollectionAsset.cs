07dafd1a2c039763d16e93366800cc2a5feea381
Vorobey <vorobyovvorobyov@gmail.com>
2024-03-07 19:12:22 +0300

    Add ViewCollectionListAsset

---
+++ Packages/UGF.Views/Runtime/ViewCollectionAsset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UGF.Builder.Runtime;
+using UGF.EditorTools.Runtime.Ids;
+using UnityEngine;
+
+namespace UGF.Views.Runtime
+{
+    public abstract class ViewCollectionAsset : ScriptableObject
+    {
+        public void GetViews(IDictionary<GlobalId, IBuilder<IView>> views)
+        {
+            if (views == null) throw new ArgumentNullException(nameof(views));
+
+            OnGetViews(views);
+        }
+
+        protected abstract void OnGetViews(IDictionary<GlobalId, IBuilder<IView>> views);
+    }
+}
